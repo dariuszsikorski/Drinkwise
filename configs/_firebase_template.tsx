aa57561aecda1514bed3112c9bfec611a195cd51
Dariusz Sikorski <dariuszsikorski.pl@gmail.com>
2019-09-10 07:25:25 +0200

    Template firebase config

---
+++ configs/_firebase_template.tsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "APPNAME.firebaseapp.com",
+  databaseURL: "https://APPNAME.firebaseio.com",
+  storageBucket: ""
+}
+
+export default firebaseConfig;
